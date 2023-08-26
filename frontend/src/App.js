f30b46ff27df487d7193a6fc61a111cbd487746e
Divya Samadhiya <samdivya1030@gmail.com>
2023-08-26 15:59:18 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
+import HomeScreen from './Screens/HomeScreen';
+
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import ProductScreen from './Screens/ProductScreen';
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">MyShop</a>
-      </header>
-      <main>list products</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">MyShop</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
